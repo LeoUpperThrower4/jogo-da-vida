db460bdad21cd4578732ccb67f129c63ec5cd42b
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2023-06-18 12:06:39 -0300

    add modal create room

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,13 +3,14 @@ import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 
 export default function Header() {
-  const [joinRoomModalOpen, setJoinRoomModalOpen] = useState(true)
+  const [joinRoomModalOpen, setJoinRoomModalOpen] = useState(false)
+  const [createRoomModalOpen, setCreateRoomModalOpen] = useState(false)
   return (
     <>
       <header className="flex justify-between items-center px-8 py-6 border-b">
         <Link href="/">Logo</Link>
         <div className="flex items-center justify-center gap-2">
-          <button className="p-2 border rounded">Criar sala</button>
+          <button className="p-2 border rounded" onClick={() => setCreateRoomModalOpen(true)}>Criar sala</button>
           <button className="p-2 border rounded" onClick={() => setJoinRoomModalOpen(true)}>Entrar em uma sala</button>
         </div>
       </header>
@@ -58,6 +59,50 @@ export default function Header() {
           </div>
         </Dialog>
       </Transition.Root>
+      <Transition.Root show={createRoomModalOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setCreateRoomModalOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                  <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                    Crie uma sala
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      Digite o seu nome
+                    </p>
+                  </div>
+                  <div className="flex flex-col my-4 text-black gap-1 h-full">
+                    <input placeholder="seu nome" className="border border-gray-800 px-2 py-1 rounded text-black" type="text" name="username" id="username" />
+                  </div>
+                  <button type="submit" className="ml-auto px-2 py-1 flex border border-gray-800 rounded flex-1 items-center justify-center text-gray-950">Criar</button>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>      
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <Header />
+      Explicação do jogo
     </main>
   )
 }
