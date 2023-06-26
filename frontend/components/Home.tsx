5aec911c0b0aa38dad85ad6267b0ca9426c74c3f
RafaelHuszcza <rafaelhuszcza@gmail.com>
2023-06-26 16:48:10 -0300

    add:homeContent

--- frontend/components/Home.tsx
+++ frontend/components/Home.tsx
@@ -1,7 +1,24 @@
 export default function Home() {
   return (
-    <main className="w-screen h-[90vh]">
-      <h1>Colocar Explicações aqui</h1>
+    <main className="flex w-screen max-h-[90vh] p-10 items-center justify-center gap-60">
+      <h1 className="text-lg"> Corrida tabuleiro</h1>
+      <p className=""> Teste sua sorte nessa corrida contra seus amigos!</p>
+      <section>
+        <details>
+          <summary>Instruções</summary>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae enim quis ipsum malesuada fermentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nec pretium leo. Etiam tristique est non porttitor fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        </details>
+        <details>
+          <summary>Tecnologias Utilizadas</summary>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae enim quis ipsum malesuada fermentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nec pretium leo. Etiam tristique est non porttitor fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+
+        </details>
+        <details>
+          <summary>Integrantes</summary>
+          xxxxxx
+        </details>
+      </section>
+      imagem fora do section
     </main>
   )
 }
